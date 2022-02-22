9a2fcd726a8cb0b30d9ab64d5adbd2cbeea457ec
성석원 <sseong@deepnoid.com>
2022-02-22 15:55:50 +0900

    인증 작업, 유저, 게시물 관계설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsModule } from './boards/boards.module';
 import { typeORMConfig } from './configs/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot(typeORMConfig),
     BoardsModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
